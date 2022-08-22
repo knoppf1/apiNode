1c350751edfb00d2411847e4bb0b0e53e870f6a4
knoppf1 <kerstnerfabiano@gmail.com>
2022-08-22 18:17:54 -0300

    Adicionando Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Testei');
+import express, {Request, Response, NextFunction} from 'express';
+
+
+const app = express();
+
+app.get('/status', (req: Request, res: Response, next: import('express').NextFunction) => {
+    res.status(200).send({ foo: 'teste 2'});
+});
+
+// Inicialização do servidor
+app.listen(3000, () => {
+    console.log('Aplicação executando na porta 3000!');
+});
+
+
